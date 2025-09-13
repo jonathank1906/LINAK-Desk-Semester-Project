8a2d9fdd6c8c4de8d5f6a65b9759aadc96f769af
jonathank1906 <jklef24@student.sdu.dk>
2025-09-13 19:18:38 +0200

    Access token and refresh token saved in cookie

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        try:
+            response = super().post(request, *args, **kwargs)
+            tokens = response.data
+
+            access_token = tokens.get('access')
+            refresh_token = tokens.get('refresh')
+
+            res = Response()
+
+            res.data = {'success':True}
+
+            res.set_cookie(
+                key='access_token',
+                value=access_token,
+                httponly=True,
+                secure=True,
+                samesite='None',
+                path='/'
+            )
+
+            res.set_cookie(
+                key='refresh_token',
+                value=refresh_token,
+                httponly=True,
+                secure=True,
+                samesite='None',
+                path='/'
+            )
+
+
+            return res
+
+        except:
+            return Response({'success':False})
+
