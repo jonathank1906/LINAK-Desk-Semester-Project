b5996de587480eb23c7518aca08ae26d937d5afa
jonathank1906 <jklef24@student.sdu.dk>
2025-09-13 14:11:21 +0200

    JWT authentication working with Postman

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'core',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
-import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
