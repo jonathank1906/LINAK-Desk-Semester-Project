c61bbcaf982834285e85a52267835d3fb0189d02
jonathank1906 <jklef24@student.sdu.dk>
2025-09-14 09:48:04 +0200

    Log in and log out working

--- frontend/src/components/private_route.jsx
+++ frontend/src/components/private_route.jsx
@@ -3,14 +3,14 @@ import { useNavigate } from 'react-router-dom';
 
 
 const PrivateRoute = ({children}) => {
-    const { isAuthenticated, loading } = useAuth();
+    const { user, loading } = useAuth();
     const nav = useNavigate();
 
     if (loading) {
         return <div>Loading...</div>;
     }
 
-    if (isAuthenticated) {
+    if (user) {
         return children;
     } else {
         nav('/login');
