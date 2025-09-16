402cf8a814ee84a22a3bd4f0d8dfcfe7267c67fc
jonathank1906 <jklef24@student.sdu.dk>
2025-09-16 12:04:47 +0200

    Seed admin login details

---
+++ backend/core/management/commands/seed_data.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = "Seed initial database data"
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        user, created = User.objects.get_or_create(username='admin')
+        user.is_staff = True
+        user.set_password('123')
+        user.save()
+        if created:
+            self.stdout.write(self.style.SUCCESS('User "test" created successfully'))
+        else:
+            self.stdout.write(self.style.WARNING('User "test" updated'))
