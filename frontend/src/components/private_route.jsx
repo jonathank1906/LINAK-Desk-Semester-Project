911b25b8de573c131d8b345ced064f559ba22a8a
Jonathan Kleffel <jklef24@student.sdu.dk>
2025-09-21 17:12:26 +0200

    Spinner during loading.

--- frontend/src/components/private_route.jsx
+++ frontend/src/components/private_route.jsx
@@ -1,9 +1,10 @@
 import { useAuth } from '../contexts/useAuth';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { Spinner } from '@/components/ui/shadcn-io/spinner';
 
 const PrivateRoute = ({ children }) => {
-    const { user, loading } = useAuth();
+    const { user, loading } = useAuth(); // Use actual loading from context
     const navigate = useNavigate();
     const [shouldRender, setShouldRender] = useState(false);
 
@@ -18,7 +19,19 @@ const PrivateRoute = ({ children }) => {
     }, [user, loading, navigate]);
 
     if (loading) {
-        return <div>Loading...</div>;
+        return (
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '100vh',
+                    width: '100vw',
+                }}
+            >
+                <Spinner variant="circle" size={64}/>
+            </div>
+        );
     }
 
     return shouldRender ? children : null;
