4d6f7aae24ce110879fc235cce12bad419824682
Jonathan Kleffel <jklef24@student.sdu.dk>
2025-12-04 12:51:42 -0800

    Minor sidebar fix.

--- frontend/src/components/nav-main.jsx
+++ frontend/src/components/nav-main.jsx
@@ -22,13 +22,23 @@ export function NavMain({
               <SidebarMenuButton
                 tooltip={item.title}
                 onClick={item.onClick}
-                className={`bg-[#CECECE] dark:bg-[#323232]
+                className={`
+                  bg-[#CECECE] dark:bg-[#323232]
                   hover:bg-[#CECECE] dark:hover:bg-[#323232]
                   ${activeSection === item.section
-                    ? 'text-white dark:text-white hover:text-white dark:hover:text-white bg-[#1a3a5a] dark:bg-[#1a3a5a] hover:bg-[#1a3a5a] dark:hover:bg-[#1a3a5a] border-2 border-[#0078d7] hover:border-[#0078d7]'
-                    : 'border-2 border-transparent hover:border-[#4e4e4e]'
-                  }`
-                }
+                    ? `
+                      text-white dark:text-white
+                      hover:text-white dark:hover:text-white
+                      bg-[#1a3a5a] dark:bg-[#1a3a5a]
+                      hover:bg-[#1a3a5a] dark:hover:bg-[#1a3a5a]
+                      active:bg-[#1a3a5a] active:text-white
+                      border-2 border-[#0078d7] hover:border-[#0078d7]
+                    `
+                    : `
+                      border-2 border-transparent hover:border-[#4e4e4e]
+                    `
+                  }
+                `}
               >
                 {item.icon && <item.icon />}
                 <span>{item.title}</span>
